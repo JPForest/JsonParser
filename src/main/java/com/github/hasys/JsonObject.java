ac8c3ea47e8b6d0c69e070e96a37bfa1dabcb527
Kirill Gaevskii <kgaevski@redhat.com>
2016-02-28 15:03:40 +0100

    Extends JsonObject API

--- src/main/java/com/github/hasys/JsonObject.java
+++ src/main/java/com/github/hasys/JsonObject.java
@@ -19,4 +19,20 @@ public class JsonObject {
     public void addValue(String key, Object value) {
         values.put(key, value);
     }
+
+    /**
+     * Return JsonObject value of member.
+     *
+     * @param name is string name of required member value
+     * @return value of member as JsonObject
+     * @throws IllegalStateException if value of member is not a JsonObject
+     */
+    public JsonObject getJsonObjectByName(String name) {
+        Object o = values.get(name);
+        if (!(o instanceof JsonObject)) {
+            throw new IllegalStateException("Member " + name + " is not a JsonObject");
+        }
+
+        return (JsonObject) o;
+    }
 }
